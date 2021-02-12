b3b8f581e7ee344ae7cc5a1639b9512df1a48309
Olivia <info@obeaty.com>
2021-02-11 21:53:55 -0600

    feat: shuffle buzzword list on load
    - shuffle through full buzzword list, not just first 24 items

--- src/App.js
+++ src/App.js
@@ -12,10 +12,32 @@ function App() {
 
   // }
 
+  const shuffledBuzzwords = buzzArr => {
+    let currentIndex = buzzArr.length,
+      temporaryValue,
+      randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(
+        Math.random() * currentIndex,
+      )
+      currentIndex -= 1
+      // console.log(array, currentIndex)
+
+      // And swap it with the current element.
+      temporaryValue = buzzArr[currentIndex]
+      buzzArr[currentIndex] = buzzArr[randomIndex]
+      buzzArr[randomIndex] = temporaryValue
+    }
+    return buzzArr.slice(0, 25)
+  }
+
   // TODO: RENDER FULL BUZZWORDS BANK
 
   // TODO: FILTER TO 24 FOR BINGO BOARD CARDS, SAVE TO BROWSER STORAGE WITH SIDE EFFECT
-  const bingoBoard = buzzwordList.slice(0, 25)
+  const bingoBoard = shuffledBuzzwords(buzzwordList)
   // console.log(bingoBoard);
 
   // TODO: SERVE SHUFFLED ARRAY BACK TO BingoGrid AND SAVE TO BROWSER STORAGE AGAIN
