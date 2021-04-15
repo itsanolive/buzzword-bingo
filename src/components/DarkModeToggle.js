f6c2f1a81294c435d92cf0b81a988832a1d5d8db
Olivia <info@obeaty.com>
2021-04-14 21:37:12 -0500

    feat: dark mode toggle in header
    https://github.com/itsanolive/buzzword-bingo/issues/5
    https://github.com/itsanolive/buzzword-bingo/issues/2

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,25 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react'
 import Icon from './Icon'
 
 // * Header dark/light mode toggle
-const DarkModeToggle = ({props}) => {
-    // const [darkMode, setDarkMode] = useState(false)
+class DarkModeToggle extends Component {
+    constructor(props) {
+        super(props)
+        this.state =  {
+            dark: this.setDark(this.props.mode)
+        }
+        this.darkToggle = this.darkToggle.bind(this)
+    }
 
-    useEffect(() => {
-        // setDarkMode(window.matchMedia("(prefers-color-scheme: dark)"))
-    })
+    setDark(mode) {
+        if ( mode === 'dark' ) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-    return (
-        <div id="DarkModeToggle">
-            <Icon name="sun"></Icon>
-            <Icon name="toggle-left"></Icon>
-            <Icon name="toggle-right"></Icon>
-            <Icon name="moon"></Icon>
-        </div>
-    )
+    darkToggle = () => {
+        if (this.state.dark === true) {
+            this.setState({
+                dark: false
+            })
+        } else {
+            this.setState({
+                dark: true
+            })
+        }
+        let container = document.getElementById('main')
+
+        if (container.classList.contains('dark')) {
+            container.classList.remove('dark')
+        } else {
+            container.classList.add('dark')
+        }
+    }
+
+    render() {
+        let lightDefault = 'mx-2 light-icon'
+        let darkDefault = 'mx-2 dark-icon'
+        let toggleDefault = 'toggle'
+        let activeStyles = ' active'
+        let inactiveStyles = ''
+        let toggleLightClasses = toggleDefault + (this.state.dark ? inactiveStyles : activeStyles)
+        let toggleDarkClasses = toggleDefault + (this.state.dark ? activeStyles : inactiveStyles)
+        let lightClasses = lightDefault + (this.state.dark ? inactiveStyles : activeStyles)
+        let darkClasses = darkDefault + (this.state.dark ? activeStyles : inactiveStyles)
+
+        return (
+            <div id="DarkModeToggle"
+                onClick={this.darkToggle}>
+                <div className={lightClasses}>
+                    <Icon name="sun"></Icon>
+                </div>
+                <div className={toggleLightClasses}>
+                    <Icon name="toggle-left"></Icon>
+                </div>
+                <div className={toggleDarkClasses}>
+                    <Icon name="toggle-right"></Icon>
+                </div>
+                <div className={darkClasses}>
+                    <Icon name="moon"></Icon>
+                </div>
+            </div>
+        )
+    }
 }
 
 // * Default to media selection, but allow user to change manually
 
 
-export default DarkModeToggle;
+export default DarkModeToggle
